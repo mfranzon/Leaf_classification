f48e87597d503b91e3e4dbd9380bfe4fbcc7ea86
mfranzon <franzonmarco94@gmail.com>
2019-02-01 18:07:52 +0100

    lda commit

--- Models/LDA.R
+++ Models/LDA.R
@@ -1,15 +1,30 @@
-leaf_lda <- lda(Species ~ ., training)
+leaf_lda <- lda(Species ~ ., leaf)
 
 #pred_lda<-predict(leaf_lda)$posterior 
 
-pred_lda <- predict(leaf_lda, test)
+pred_lda <- predict(leaf_lda, leaf)
 
 #cf <- confusionMatrix(pred_lda$class, test$Species)
 
-tab <- table(Predicted = pred_lda$class, Actual = test$Species)
+tab_lda <- table(Predicted = pred_lda$class, Actual = leaf$Species)
 
-misc <-1-sum(diag(tab))/sum(tab)
+misc <-1-sum(diag(tab_lda))/sum(tab_lda)
 
-plot(pred_lda$x[,1], type="n", xlim=c(0,30), ylab=c("LDA Axis 1"))
+plot(pred_lda$x[,1],pred_lda$x[,2], col = leaf$Species) # make a scatterplot
 
-text(pred_lda$x[,1], row.names(hab_std),  col=c(as.numeric(hab.class)+10)
+
+#lda with training
+tleaf_lda <- lda(Species ~ ., training)
+
+#pred_lda<-predict(leaf_lda)$posterior 
+
+tpred_lda <- predict(leaf_lda, test)
+
+#cf <- confusionMatrix(pred_lda$class, test$Species)
+
+ttab_lda <- table(Predicted = tpred_lda$class, Actual = test$Species)
+
+misc <-1-sum(diag(ttab_lda))/sum(ttab_lda)
+
+plot(tpred_lda$x[,1],tpred_lda$x[,2], col = leaf$Species) # make a scatterplot
+
