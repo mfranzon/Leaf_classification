b6fa68cb0434f0ac25018359b0eebdd14b5a8ec7
mfranzon <43796979+mfranzon@users.noreply.github.com>
2019-02-02 11:10:42 +0100

    Update RandomForest.R

--- Models/RandomForest.R
+++ Models/RandomForest.R
@@ -1,13 +1,11 @@
-leaf_rf <- randomForest(training$Species ~ training$Eccentricity + training$Elongation + training$`Aspect Ratio` + training$Solidity + training$`Stochastic Convexity` + training$`Isoperimetric Factor` + training$`Maximal Indentation Depth` + training$Lobedness + training$`Average Intensity` + training$`Average Contrast` + training$Smoothness + training$`Third moment` + training$Uniformity + training$Entropy, data = training)
-
 leaf_rf <- randomForest(Species~., data = training , ntree = 2000)
 
 importance(leaf_rf)
 
-varImpPlot(leaf_rf)
+varImpPlot(leaf_rf) #importance of predictors
 
 pred_rf <- predict(leaf_rf, test)
 
 tab_rf <- table(pred_rf, test$Species)
 
-misc <-1-sum(diag(tab_rf))/sum(tab_rf)
+misc <-1-sum(diag(tab_rf))/sum(tab_rf) #misclassification error
