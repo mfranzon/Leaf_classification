a6d234265d7d92b94b98964f7d61ec98c72c2e4c
mfranzon <43796979+mfranzon@users.noreply.github.com>
2019-02-02 11:11:18 +0100

    Update SVM.R

--- Models/SVM.R
+++ Models/SVM.R
@@ -2,11 +2,11 @@ tmod<-tune(svm, train.x = training[,2:15], train.y = training[,1], ranges = list
 
 model<- tmod$best.model
 
-mod_svm <- svm(Species ~., training, method = "C-classification", kernel = "radial", cost = 32, gamma = 0.07)
+mod_svm <- svm(Species ~., training, method = "C-classification", kernel = "radial", cost = 32, gamma = 0.07) #best model
 
 pred_svm <- predict(mod_svm, test)
 
 tab <- table(Predicted = pred, Actual = test[,1])
 
-misc <-1-sum(diag(tab))/sum(tab)
+misc <-1-sum(diag(tab))/sum(tab) #misclassification error
 
